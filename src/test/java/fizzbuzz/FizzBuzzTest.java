5b973afa6bd2a97110d22bc6710a081e942e1ccf
Tony Baines <tony.baines@bt.com>
2012-12-04 15:51:22 +0000

    Starter project for FizzBuzz TDD exercise

---
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,18 @@
+package fizzbuzz;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+/**
+ * For numbers divisible 3 say 'Fizz'
+ * For numbers divisible 5 say 'Buzz'
+ * For numbers divisible 3 and 5 say 'FizzBuzz'
+ * For all other numbers just repeat the number 
+ */
+public class FizzBuzzTest {
+	@Test
+	public void startHere() {
+		fail("No test yet");
+	}
+}
